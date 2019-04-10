44545a425d549bb14b2ddcc0cb73029baccf1d74
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-04-10 12:11:46 +0300

    Use fixtures to share test data between tests

--- test/test_example.py
+++ test/test_example.py
@@ -1,10 +1,19 @@
+from sum import sum
 import pytest
 
 
-def sum(num1, num2):
-    """It returns sum of two numbers"""
-    return num1 + num2
+@pytest.fixture
+def get_sum_test_data():
+    return [(1, 2, 3), (3, 5, 8), (-2, 2, 0)]
 
 
-def test_sum():
-    assert sum(1, 2) == 3
+def test_sum_output_type():
+    assert type(sum(1, 2)) is int
+
+
+def test_sum(get_sum_test_data):
+    for data in get_sum_test_data:
+        num1 = data[0]
+        num2 = data[1]
+        expected = data[2]
+        assert sum(num1, num2) == expected
